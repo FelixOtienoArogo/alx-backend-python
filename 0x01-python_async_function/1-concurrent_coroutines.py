8499d242f488ffc6db9eac2a36c06d21e2dc1f44
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-02 17:44:14 +0300

    1-concureent coroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,2 +1,28 @@
 #!/usr/bin/env python3
-"""
+"""wait_n."""
+
+
+import asyncio
+import random
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Description.
+
+    -----------
+    wait_n should return the list of all the delays (float values).
+    The list of the delays should be in ascending order without using
+    sort() because of concurrency.
+    """
+    out: List[float] = []
+    i: int = 0
+    while (i < n):
+        dig = await wait_random(max_delay)
+        out.append(dig)
+        i += 1
+    return out

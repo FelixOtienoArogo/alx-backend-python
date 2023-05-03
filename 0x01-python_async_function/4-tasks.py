57ac1a991be26abfb2b46e2fc16e5d605b0f4e65
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-03 22:47:13 +0300

    new method

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -19,17 +19,15 @@ async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
     The list of the delays should be in ascending order without using
     sort() because of concurrency.
     """
+    buf: List[float] = []
     out: List[float] = []
-    i: int = 0
-    while (i < n):
+
+    for i in range(n):
         task = task_wait_random(max_delay)
-        await task
-        dig = task.result()
-        for j in range(len(out)):
-            if dig < out[j]:
-                out.insert(j, dig)
-                break
-        else:
-            out.append(dig)
-        i += 1
+        task.add_done_callback(lambda x: out.append(x.result()))
+        buf.append(task)
+
+    for j in buf:
+        await j
+
     return out

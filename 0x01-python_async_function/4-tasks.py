a6e216f3966b9b0328a404ab6726f6eb1cace495
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-05 20:17:57 +0300

    try

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python3
-"""wait_n."""
 
+'''
+Let's execute multiple coroutines at the same time with async
+'''
 
 import asyncio
 import random
 from typing import List
-
-
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int, max_delay: int = 10) -> List[float]:
-    """
-    Description.
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    Call wait_random n times
+    '''
 
-    -----------
-    wait_n should return the list of all the delays (float values).
-    The list of the delays should be in ascending order without using
-    sort() because of concurrency.
-    """
     result = []
-    
-    tasks = [task_wait_random(max_delay) for i in range(n)]
 
-    for sort in asyncio.as_completed(tasks):
+    delays = [task_wait_random(max_delay) for time in range(n)]
+
+    for sort in asyncio.as_completed(delays):
         val = await sort
         result.append(val)
-
     return result

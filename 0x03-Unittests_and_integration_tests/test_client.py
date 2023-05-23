f54695b3265c6d48923b37be2a975a7009acc9dc
FelixOtienoArogo <felixarogo@gmail.com>
2023-05-23 11:19:22 +0300

    Parameterize

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -45,3 +45,13 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(test_return, ["holberton"])
             mock_get.assert_called_once
             mock_buc.assert_called_once
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """To unit-test GithubOrgClient.has_license."""
+        test_client = GithubOrgClient("holberton")
+        test_return = test_client.has_license(repo, license_key)
+        self.assertEqual(test_return, expected)
